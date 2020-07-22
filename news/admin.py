127c734b114fec1ceed9f2be9444d82695caae9a
Mihail7318 <50657490+Mihail7318@users.noreply.github.com>
2020-07-22 10:59:02 +0400

    Add files via upload

---
+++ news/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import article, covid
+
+admin.site.register(article)
+admin.site.register(covid)
+# Register your models here.
+
